b33730139dc5a4fb10e32edf5ec16c1f6bdaef49
Kiran R <95201907+the-devil666@users.noreply.github.com>
2023-02-23 18:45:12 +0530

    WebSocket-master

---
+++ WebSocket-master/src/main/java/com/pws/spring/ws/api/SpringBootWebsocketApplication.java
@@ -0,0 +1,16 @@
+package com.pws.spring.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@SpringBootApplication
+@EnableWebSocket
+public class SpringBootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketApplication.class, args);
+	}
+
+}
+
